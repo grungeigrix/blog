d95e71aed3176244f4c8135cb19629a5b50c5773
grungeigrix <72521952+grungeigrix@users.noreply.github.com>
2021-04-24 20:17:39 +0300

    Add Postgres

--- bl/settings.py
+++ bl/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'bl.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD': '2254',
+        'HOST': '127.0.0.1',
+        'PORT':'5432',
     }
 }
 
@@ -123,7 +127,7 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/magos/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
