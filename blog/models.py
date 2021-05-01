8c2ec2b056da8a54da4dda13a43b888344173fe7
grungeigrix <72521952+grungeigrix@users.noreply.github.com>
2021-05-01 21:53:48 +0300

    'Finish'

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,13 @@ from django.db import models
 
 
 class Post(models.Model):
-	date = models.DateTimeField()
 	title = models.CharField('Title', max_length=140)
+	date = models.DateTimeField()	
 	image = models.ImageField(upload_to='event_images/')
 	text = models.CharField(max_length=300)
+
+	def get_gummary(self):
+		return self.text[:70]
+
+	def __str__(self):
+		return self.title
